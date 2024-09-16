7f39d9c87df314689f65f1677f7f4382f14bbec2
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-09-16 17:59:35 +0300

    Update Vector.h

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -10,12 +10,18 @@ class Vector {
 private:
     int *arr = nullptr;
     int size = 0 ;
+    int maxSize ;
 public:
     Vector();
     Vector(int maxSize);
     int get(int indx);
     void set(int indx , int val);
     void print();
+    int front();
+    int back();
+    void push_back(int val);
+    void inCapacity();
+    void insert(int indx , int val);
     int find(int val);
     ~Vector();
 };
