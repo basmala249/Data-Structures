ef1f66ba81fb0ac93f39a9089a445b458cef87f5
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2024-09-16 22:33:14 +0300

    Update Vector.cpp

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -6,45 +6,48 @@
 #include <iostream>
 
 using namespace std;
-Vector ::Vector() {
+
+template<class T>
+Vector<T> ::Vector() {
     size = 0;
     maxSize = 200;
-    arr = new int[maxSize];
+    arr = new T[maxSize];
 }
-
-Vector ::Vector(int maxSize) {
+template<class T>
+Vector<T> ::Vector(int maxSize) {
     size = 0;
     this -> maxSize = maxSize;
     if(maxSize < 0)maxSize = 1;
-    arr = new int[maxSize];
+    arr = new T[maxSize];
 }
-
-int Vector::get(int indx) {
+template<class T>
+T Vector<T>::get(int indx) {
     if(indx < 0 or indx >= size){
-        return -1;
+        throw out_of_range("index not found");
     }
     return arr[indx];
 }
-
-void Vector::set(int indx, int val) {
+template<class T>
+void Vector<T>::set(int indx, T val) {
     if(indx < 0 or indx > size){
         return ;
     }
+    if(size == maxSize)inCapacity();
     if(indx == size)size++;
     arr[indx] = val ;
 }
-
-int Vector::front() {
+template<class T>
+T Vector<T>::front() {
     if(size > 0)return arr[0];
-    return -1;
+    throw out_of_range("Empty");
 }
-
-int Vector::back() {
+template<class T>
+T Vector<T>::back() {
     if(size > 0)return arr[size - 1];
-    return -1;
+    throw out_of_range("Empty");
 }
-
-void Vector::push_back(int val) {
+template<class T>
+void Vector<T>::push_back(T val) {
     if(size < maxSize){
         arr[size++] = val;
         return ;
@@ -53,8 +56,8 @@ void Vector::push_back(int val) {
     inCapacity();
     arr[size++] = val;
 }
-
-void Vector::insert(int indx, int val) { //O(n)
+template<class T>
+void Vector<T>::insert(int indx, T val) { //O(n)
     if(indx < 0 or indx > size)return;
     if(size == maxSize)inCapacity();
     for(int i = size ; i > indx ; --i){
@@ -63,9 +66,9 @@ void Vector::insert(int indx, int val) { //O(n)
     arr[indx] = val;
     size++;
 }
-
-void Vector::inCapacity() {
-    int *arr2 = (new int[maxSize * 2]);
+template<class T>
+void Vector<T>::inCapacity() {
+    T *arr2 = (new T[maxSize * 2]);
     for(int i = 0 ; i < size ; ++i) {
         arr2[i] = arr[i];
     }
@@ -73,22 +76,27 @@ void Vector::inCapacity() {
     swap(arr2 , arr);
     delete [] arr2;
 }
-
-void Vector::print() {
+template<class T>
+void Vector<T>::print() {
     for(int i = 0 ; i < size ; ++i){
         cout << arr[i] << " ";
     }
     cout << '\n';
 }
-
-int Vector::find(int val) {
+template<class T>
+T Vector<T>::find(T val) {
     for(int i = 0 ; i < size ; ++i){
         if(arr[i] == val)return i;
     }
-    return -1;
+    throw out_of_range("Not Found");
 }
-
-Vector ::~Vector() {
+template<class T>
+T Vector<T>::operator[](int i) {
+    if(i < 0 or i >= size)return -1;
+    return arr[i];
+}
+template<class T>
+Vector<T> ::~Vector() {
     delete [] arr;
     arr = nullptr;
 }
